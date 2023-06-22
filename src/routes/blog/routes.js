0c23691ea10c02929d7d7c966c29c298dd639896
Pavel <korolevpavel2008@gmail.com>
2023-06-22 22:37:28 +0400

    added post 1

--- src/routes/blog/routes.js
+++ src/routes/blog/routes.js
@@ -3,5 +3,15 @@ export const routes = [
         id: 1,
         title:'My first blog post',
         body:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, ut sequi. Odio natus iste consequuntur architecto cupiditate. Est molestias similique laborum corporis sit nostrum unde doloremque fuga. A, amet doloremque.'
+    },
+    {
+        id: 2,
+        title:'My second blog post',
+        body:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, ut sequi. Odio natus iste consequuntur architecto cupiditate. Est molestias similique laborum corporis sit nostrum unde doloremque fuga. A, amet doloremque.'
+    },
+    {
+        id: 3,
+        title:'My third blog post',
+        body:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil, ut sequi. Odio natus iste consequuntur architecto cupiditate. Est molestias similique laborum corporis sit nostrum unde doloremque fuga. A, amet doloremque.'
     }
 ]
