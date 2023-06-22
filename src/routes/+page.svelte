0aaf00cd484521497b7587023c1eccb2318800d4
Pavel <korolevpavel2008@gmail.com>
2023-06-22 12:58:02 +0400

    added structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Королев Павел</title>
+</svelte:head>
+
+<div class='container'>
+    <div class='info'>
+        <div class='animate-background'>
+            <div class='container info-container'>
+                <div class='info-text'></div>
+                    <h1>
+                        <p class='animate-text-1'>Lorem, ipsum dolor sit,</p>
+                        <p class='text-2'>Lorem, ipsum dolor sit amet.</p>
+                    </h1>
+            </div>
+            <h2>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod rem esse accusamus
+                ratione officia tempore
+                natus vero, suscipit quis obcaecati odit, amet harum. Nihil tempora nostrum 
+            </h2>
+            <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+            <div style='color:black' class='scroll-button animate-scroll' tabindex='0'>
+                <a href='/about'>Click Here</a>
+                <div class='arrow'>→</div>
+            </div>
+        </div>
+    </div>
+</div>
