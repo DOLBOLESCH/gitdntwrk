01e1cd438baca0d8c01a8b41565cf2e901848f3b
Pavel <korolevpavel2008@gmail.com>
2023-06-22 14:23:02 +0400

    added css into project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,14 +12,80 @@
                         <p class='text-2'>Lorem, ipsum dolor sit amet.</p>
                     </h1>
             </div>
-            <h2>
+            <h2 class="subtitle">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod rem esse accusamus
                 ratione officia tempore
                 natus vero, suscipit quis obcaecati odit, amet harum. Nihil tempora nostrum 
             </h2>
             <div style='color:black' class='scroll-button animate-scroll' tabindex='0'>
-                <a href='/about'>Click Here</a><div class='arrow'>→</div>
+                <a href='/about'>Click Here</a>
+                <div class='arrow'>→</div>
             </div>
         </div>
     </div>
 </div>
+
+<style>
+.info {
+    text-align: center;
+    position: relative;        
+}
+
+.info-container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: left;
+    align-items: start;
+}
+
+.container {
+    max-width: 1400px;
+    margin: 190px 0 0 2.5rem;
+}
+
+.info h1 {
+    font-size: 4rem;
+    margin: 0;
+}
+
+.info h1 p {
+    position: relative;
+    font: inherit;
+    margin: 0;
+    text-align: left;
+    line-height: 130%;
+    width: fit-content;
+    font-weight: 800;
+    letter-spacing: -0.03em;
+}
+
+.info h2 {
+    max-width: 65ch;
+    font-weight: 200;
+    margin-top: 0.50rem;
+}
+
+.background-div {
+    color: #ecebe8;
+    width: 100%;
+    height: 56vh;
+    margin: -205px auto;
+}
+
+.info .info-text {
+    position: relative;
+}
+
+.scroll-button {
+    display: flex;
+    border: none;
+    outline: none;
+    cursor: pointer;
+}
+
+.scroll-button .arrow {
+    margin-left: 0.50rem;
+}
+</style>
