8b0d515cb54f10c469856e76d4a8ba3249a45b62
Pavel <korolevpavel2008@gmail.com>
2023-06-23 13:54:41 +0400

    added second blogpost

---
+++ src/routes/blog/post/2/+page.svelte
@@ -0,0 +1,3 @@
+<h1>My Second Blog Post</h1>
+
+<p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus unde, error nihil beatae cum dolor dolorem maiores similique hic aliquam deleniti vitae delectus quidem nemo tempora, consequuntur in voluptatum? Tenetur.</p>
