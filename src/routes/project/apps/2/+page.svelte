3e8ce19a92666bc40d6f39598acd68f48330b058
Pavel <korolevpavel2008@gmail.com>
2023-06-25 23:46:54 +0400

    added 2 proj concept

---
+++ src/routes/project/apps/2/+page.svelte
@@ -0,0 +1,42 @@
+<table>
+    <thead>
+    <tr>
+        <th>Название</th>
+        <th>Cумма</th>
+        <th>Действия</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    {#each expenses as expense, i}
+    <tr>
+        <td><div>{expense.name}</div></td>
+        <td><div>{expense.amount}</div></td>
+        <td><button on:click={() => deleteExpense(i)}>Убрать</button></td>
+    </tr>
+    {/each}
+    </tbody>
+</table>
+
+<script>
+    let expenses = [];
+
+    let newName = '';
+    let newAmount = '';
+
+    function addExpense() {
+        const obj = {
+            name: newName,
+            amount: parseInt(newAmount),
+        };
+        expenses.push(obj);
+        expenses = expenses;
+        newName = '';
+        newAmount = '';
+    }
+
+    function deleteExpense(index) {
+        expenses.splice(index, 1);
+        expenses = expenses;
+    }
+</script>
